17ceee765cd59efe9e27d337e726da52311cc5df
bongbong0331 <faker0331@naver.com>
2022-12-12 15:08:37 +0900

    Spring Boot & JPA 21 전통적인 로그인 처리

--- src/main/java/com/tencoding/blog/api/UserApiController.java
+++ src/main/java/com/tencoding/blog/api/UserApiController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,4 +47,10 @@ public class UserApiController {
 		
 		return new ResponseDto<User>(HttpStatus.OK, principal);  // 자바 OBJECT --> JSON 형식으로 
 	}
+	
+	
+
+	
+	
+	
 }
--- src/main/java/com/tencoding/blog/controller/UserController.java
+++ src/main/java/com/tencoding/blog/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.tencoding.blog.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+	@Autowired
+	HttpSession session;
 
 	@GetMapping("/login-form")
 	public String loginForm() {
@@ -21,4 +27,29 @@ public class UserController {
 		// 프리픽스 /WEB-INF/views/user/join_form.jsp
 		return "user/join_form";
 	}
+
+	// 기존 스프링에서 로그아웃 처리는 -- 따로 정리 !! 
+	@GetMapping("/logout")
+	public String logout() {
+
+		HttpSession httpSession = session;
+		httpSession.invalidate(); // 로그아웃 처리
+		
+		System.out.println("11111111111111");
+		return "/index";
+	}
+	
+//	// 기존 스프링에서 로그아웃 처리는 -- 따로 정리 !! 
+//		@GetMapping("/logout")
+//		public String logout() {
+//
+//			HttpSession httpSession = session;
+//			httpSession.invalidate(); // 로그아웃 처리
+//			
+//			System.out.println("11111111111111");
+//			return "/index";
+//		}
+	
+	
+	
 }
