8d5dceea57f89a23fe8880cf4f33735fd260d89b
bongbong0331 <faker0331@naver.com>
2022-12-19 12:14:02 +0900

    Spring Boot & JPA 43강 페이징 처리 - 2

--- src/main/java/com/tencoding/blog/controller/BoardController.java
+++ src/main/java/com/tencoding/blog/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.tencoding.blog.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,12 +36,45 @@ public class BoardController {
 			@PageableDefault(size = 3, sort = "id", direction = Direction.DESC) Pageable pageable) {
 
 		Page<Board> boards = boardService.getBoardList(pageable);
-
+		int PAGENATION_BLOCK_COUNT = 3;
+		
 //		page.first == true, false  < -- 첫번째 페이지 true
 //				page.last == true, false  < -- 마지막 페이지 last 
 
-		model.addAttribute("boards", boards); // jsp 파일에서 model 추상화객체를 이용하여 컨트롤러에서 내려 준 데이터를 접근할 수 있다.
+//		model.addAttribute("boards", boards); // jsp 파일에서 model 추상화객체를 이용하여 컨트롤러에서 내려 준 데이터를 접근할 수 있다.
 
+		// 1. 현재 페이지 앞 뒤로 2칸씩 보이기
+		// 2. 현재 페이지 active 처리 하기
+		// 3. 페이지 숫자를 눌렀을 경우 해당 페이지로 화면 이동하기
+		
+		// 총 게시물에서 화면에 보여줄 게시물을 나누기 하여 계산을 하면 총 몇페이지가 나오는지 알 수 있다.
+		System.out.println(">>>>> 화면에 보여줄 게시글의 갯수이다! " + boards.getSize());
+		System.out.println(">>>>> 전체 페이지 크기 " + boards.getTotalPages());
+		System.out.println(">>>> 현재 페이지 번호 " + boards.getPageable().getPageNumber());
+		
+		int nowPage = boards.getPageable().getPageNumber() + 1;
+		// 123456789
+//		int startPageNumber = nowPage - 2; 
+		// 블럭 숫자 (페이지번호) 에는 - 값을 보여주면 안된다.
+		int startPageNumber = Math.max(nowPage - PAGENATION_BLOCK_COUNT, 1);
+		int endPageNumber = Math.min(nowPage + PAGENATION_BLOCK_COUNT , boards.getTotalPages());
+		
+		System.out.println("시작해야하는번호" + startPageNumber);
+		System.out.println("마지막에 보여줄 번호" + endPageNumber);
+//		블럭 숫자에는 - 값을 보여주면 안된다.
+
+		ArrayList<Integer> pageNumbers = new ArrayList<>();
+		for(int i = startPageNumber; i < endPageNumber; i++) {
+			pageNumbers.add(i);
+		}
+		
+		model.addAttribute("boards", boards);
+		model.addAttribute("nowPage",nowPage);
+		model.addAttribute("startPageNumber",startPageNumber);
+		model.addAttribute("endPageNumber",endPageNumber);
+		model.addAttribute("pageNumbers",pageNumbers);
+		
+		
 		return "index";
 	}
 
