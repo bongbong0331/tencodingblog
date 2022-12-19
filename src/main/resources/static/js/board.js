9a345e19872ffda29c6508b90444163c060454e1
bongbong0331 <faker0331@naver.com>
2022-12-19 16:57:03 +0900

    Spring Boot & JPA 45강 XSS 예방

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -8,28 +8,32 @@ let index = {
 		$("#btn--delete").bind("click", () => {
 			this.deleteById();
 		});
-		
+
 		$("#btn--update").bind("click", () => {
 			this.update();
 		});
-		
+
 		$("#btn-reply-save").bind("click", () => {
 			this.replySave();
 		});
-		
+
 	},
 
 
 
 	save: function() {
+		let xCheckTitle = XSSCheck($("#title").val());
+		console.log(xCheckTitle);
+
 
 		let data = {
-			title: $("#title").val(),
+			title: xCheckTitle,
 			content: $("#content").val()
 		};
 
 
 		// ajax 통신 요청
+
 		$.ajax({
 			type: "POST",
 			url: "/api/board",
@@ -46,6 +50,8 @@ let index = {
 			alert("등록실패ㅜㅜ" + error.responseJSON.message);
 		});
 
+
+
 		/**
 		람다식 -
 		fail((error) => {
@@ -55,11 +61,11 @@ let index = {
 		 */
 
 	},
-	
-	
+
+
 	deleteById: function() {
 
-		let id = $("#board-id").text();      // text val 차이 
+		let id = $("#board-id").val();      // text val 차이 
 
 		// 통신 ---> ajax
 		$.ajax({
@@ -76,37 +82,37 @@ let index = {
 		});
 
 	},
-	
-	update: function(){
-		
+
+	update: function() {
+
 		// html 태그에 직접 속성을 정의할 수 있다. 규칙은 data-*
 		// data-* 값을 가지고 오기 위해서 Jquery --> (선택자).arrt("data-[id]")
 		let boardId = $("#board-id").attr("data-id");
-		
+
 		let data = {
-			title : $("#title").val(),
-			content : $("#content").val()
-		} 
-		
+			title: $("#title").val(),
+			content: $("#content").val()
+		}
+
 		$.ajax({
 			type: "PUT",
 			url: "/api/board/" + boardId,
-			data : JSON.stringify(data),
-			contentType : "application/json; charset=utf-8",
-			dataType : "json"
-		}).done(function(data, textStatus, xhr){
-			if(data.status){
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
+		}).done(function(data, textStatus, xhr) {
+			if (data.status) {
 				alert("글 수정이 완료 되었습니다");
 				location.href = "/";
 			}
-		}).fail(function(error){
+		}).fail(function(error) {
 			console.log(error);
 			alert("글 수정이 실패 하였습니다");
 		});
 	},
-	
-	
-	replySave : function() {
+
+
+	replySave: function() {
 
 		let replyData = {
 			boardId: $("#board-id").val(),    // fk (board pk)
@@ -133,28 +139,36 @@ let index = {
 			alert("댓글작성에 실패하여어어었습니다");
 		});
 
-},
-		replyDelete : function(boardId, replyId){
-			
-			$.ajax({
-				type: 'DELETE',
-				url: `/api/board/${boardId}/reply/${replyId}`,
-				dataType: 'json'
-			}).done(function(resData){
-				if(resData.status == "OK"){
-					alert(" 댓글 삭제 성공 하였습니다.");
-					location.href = `/board/${boardId}`
-				}
-				
-			}).fail(function(error){
-				alert("댓글 삭제 실퍃햏ㅎ")
-			});			
-			
-			
-		}
+	},
+	replyDelete: function(boardId, replyId) {
 
+		$.ajax({
+			type: 'DELETE',
+			url: `/api/board/${boardId}/reply/${replyId}`,
+			dataType: 'json'
+		}).done(function(resData) {
+			if (resData.status == "OK") {
+				alert(" 댓글 삭제 성공 하였습니다.");
+				location.href = `/board/${boardId}`
+			}
 
-
+		}).fail(function(error) {
+			alert("댓글 삭제 실퍃햏ㅎ")
+		});
+	}
 }
 
+
+function XSSCheck(str, level) {
+	if (level == undefined || level == 0) {
+		str = str.replace(/\<|\>|\"|\'|\%|\;|\(|\)|\&|\+|\-/g, "홍길동맨");
+	} else if (level != undefined && level == 1) {
+		str = str.replace(/\</g, "&lt;");
+		str = str.replace(/\>/g, "&gt;");
+	}
+	return str;
+}
+
+
 index.init();
+
